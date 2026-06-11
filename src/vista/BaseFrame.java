c9b6935037865a4980a33fe09c87f25131a38466
CarlosMatute-dev <carlo.jesum06@gmail.com>
2026-06-11 17:18:05 -0400

    todo uwu

---
+++ src/vista/BaseFrame.java
@@ -0,0 +1,13 @@
+package vista;
+import javax.swing.JFrame;
+import modelo.SistemaDVD;
+public abstract class BaseFrame extends JFrame {
+    protected SistemaDVD sistema;
+    public BaseFrame(String titulo, SistemaDVD sistema) {
+        super(titulo);
+        this.sistema = sistema;
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+    }
+    public abstract void mostrar();
+}
