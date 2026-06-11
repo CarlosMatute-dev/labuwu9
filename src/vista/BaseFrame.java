42d93f869de5d98ffc87ec4d295c023dc8df2316
CarlosMatute-dev <carlo.jesum06@gmail.com>
2026-06-11 17:28:01 -0400

    base

--- src/vista/BaseFrame.java
+++ src/vista/BaseFrame.java
@@ -1,8 +1,10 @@
 package vista;
 import javax.swing.JFrame;
 import modelo.SistemaDVD;
+
 public abstract class BaseFrame extends JFrame {
     protected SistemaDVD sistema;
+    
     public BaseFrame(String titulo, SistemaDVD sistema) {
         super(titulo);
         this.sistema = sistema;
